b07d6b928d74cc8e5a19a1db2412aa7db472b5f3
Fer <Fernandapez77@gmail.com>
2026-06-29 07:23:07 -0600

    configure initial project and test server

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5100;
 
+app.get('/', (req, res) => {
+    res.send('Hello BookStore API');
+});
+
 app.listen(PORT, () => {
-    console.log('Hello World');
     console.log(`Server running on port ${PORT}`);
 });
