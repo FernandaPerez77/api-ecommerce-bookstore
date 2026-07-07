06ca94dd2abdd35dbafd4c6f6011d6b333521f97
Fer <Fernandapez77@gmail.com>
2026-07-06 23:13:57 -0600

    Update index routes for books and users only

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/database');
-const swaggerUi=require('swagger-ui-express');
-const swaggerSpec=require('./src/docs/swagger');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./src/docs/swagger');
 
 dotenv.config();
 
@@ -12,16 +12,19 @@ const app = express();
 
 app.use(express.json());
 
+// Routes
 app.use('/api/books', require('./src/routes/bookRoutes'));
 app.use('/api/users', require('./src/routes/userRoutes'));
+
+// Swagger Documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello BookStore API');
 });
 
 const PORT = process.env.PORT || 5100;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
