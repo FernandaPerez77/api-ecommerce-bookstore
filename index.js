22252f873bddb8feed569e2ee67e374eeed972f4
Fer <Fernandapez77@gmail.com>
2026-07-06 23:05:30 -0600

    Complete BookStore CRUD and routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/database');
-const swaggerUi=require('swagger-ui-express');
-const swaggerSpec=require('./src/docs/swagger');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./src/docs/swagger');
 
 dotenv.config();
 
@@ -18,12 +18,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/carts', require('./src/routes/cartRoutes'));
 app.use('/api/orders', require('./src/routes/orderRoutes'));
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello BookStore API');
 });
 
 const PORT = process.env.PORT || 5100;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
